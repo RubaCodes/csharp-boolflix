8b95dd6c34edfe036fbc5a8ec10e4a66b6647dd7
RubaCodes <rubacodes@gmail.com>
2022-10-12 10:48:29 +0200

    scaffold iniziale progetto

---
+++ Controllers/EditorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_boolflix.Controllers
+{
+    public class EditorController : Controller
+    {
+        BoolflixContext _context;
+        public EditorController() {
+                _context = new BoolflixContext();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Movies()
+        {
+            return View();
+        }
+        public IActionResult TvSeries()
+        {
+            return View();
+        }
+        public IActionResult Genres()
+        {
+            return View();
+        }
+
+        //Features
+        public IActionResult Features()
+        {
+            List<Feature> features = _context.Features.ToList();
+            return View(features);
+        }
+
+
+        public IActionResult Actors()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/FeatureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_boolflix.Controllers
+{
+    public class FeatureController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
