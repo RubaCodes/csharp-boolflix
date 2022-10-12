d88bff3ed773c347e4c3542bbc4ee53729729970
RubaCodes <rubacodes@gmail.com>
2022-10-12 17:25:27 +0200

    fin dei null su create Movie

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -10,6 +10,7 @@ namespace csharp_boolflix.Controllers
         {
             _context = new BoolflixContext();
         }
+        [HttpGet]
         public IActionResult Create()
         {
             MovieExtended movieEx = new MovieExtended();
@@ -17,6 +18,22 @@ namespace csharp_boolflix.Controllers
             movieEx.Actors = _context.Actors.ToList();
             movieEx.Genres = _context.Genres.ToList();
             return View(movieEx);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(MovieExtended data)
+        {
+            if (!ModelState.IsValid)
+            {
+                data.Features = _context.Features.ToList();
+                data.Actors = _context.Actors.ToList();
+                data.Genres = _context.Genres.ToList();
+                return View(data);
+            }
+            Movie movie = new Movie();
+            //data.Features = _context.Features.Where(x => data.SelectedFeatures.Contains(x.Id)).toList();
+            //data.Actors = _context.Actors.Where(x => data.SelectedActors.Contains(x.Id)).toList();
+            return RedirectToAction("Movies", "Editor");
         }
     }
 }
--- Models/MediaInfo.cs
+++ Models/MediaInfo.cs
@@ -4,14 +4,14 @@
     public bool IsNew { get; set; }
 
     public int? MovieId { get; set; }
-    public Movie Movie { get; set; }
+    public Movie? Movie { get; set; }
 
     public int? TvSeriesId { get; set; }
-    public TvSeries Serie { get; set; }
-    public List<Actor> Cast { get; set; }
+    public TvSeries? Serie { get; set; }
+    public List<Actor>? Cast { get; set; }
 
-    public List<Genre> Genres { get; set; }
-    public List<Feature> Features { get; set; }
+    public List<Genre>? Genres { get; set; }
+    public List<Feature>? Features { get; set; }
 
 
 }
