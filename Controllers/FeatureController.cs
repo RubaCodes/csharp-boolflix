869c56dce10c7e88eaa812cbbbdc0bfe404f656c
RubaCodes <rubacodes@gmail.com>
2022-10-12 12:14:41 +0200

    completata crud features

--- Controllers/FeatureController.cs
+++ Controllers/FeatureController.cs
@@ -26,5 +26,32 @@ namespace csharp_boolflix.Controllers
             _context.SaveChanges();
             return RedirectToAction("Features","Editor");
         }
+        //update
+
+                [HttpGet]
+        public IActionResult Update(int id)
+        {
+            Feature feature =_context.Features.Find(id);
+            return View(feature);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update( Feature feature)
+        {
+            if (!ModelState.IsValid) {
+                return View(feature);
+            }
+            _context.Features.Update(feature);
+            _context.SaveChanges();
+            return RedirectToAction("Features","Editor");
+        }
+        //delete
+        [HttpPost]
+        public IActionResult Delete(int id){
+            Feature feature = _context.Features.Find(id);
+            _context.Features.Remove(feature);
+            _context.SaveChanges();
+            return RedirectToAction("Features", "Editor");
+        }
     }
 }
